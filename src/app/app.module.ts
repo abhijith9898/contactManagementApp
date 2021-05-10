25f1bdda227efc63a410e1f112afa18280287fff
Binoy Samuel <binoy.samuel@xcubated.in>
2021-05-10 19:18:46 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {PasswordModule} from 'primeng/password';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule,PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
